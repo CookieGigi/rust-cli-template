757a62fd2a4b596d72d8d0c10ab42c93d0982cb3
Cookiegigi <gcreusot@pm.me>
2024-04-20 15:21:38 +0700

    Comments

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use rust_cli_template::cli::CliArgs;
 use tracing_log::AsTrace;
 
 fn main() {
+    // Get command line arguments
     let args = CliArgs::parse();
 
+    // Initialize trace
     tracing_subscriber::fmt()
         .with_max_level(args.verbose.log_level_filter().as_trace())
         .init();
