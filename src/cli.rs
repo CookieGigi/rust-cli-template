af50e9a6129479d255d1874e41ee199c8b9b780d
Cookiegigi <gcreusot@pm.me>
2024-04-20 14:55:42 +0700

    Setup log (trace) management with tracing

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,12 @@
 //! Cli Arguments Parsing
 
 use clap::Parser;
+use clap_verbosity_flag::Verbosity;
 
 /// Cli arguments
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-pub struct CliArgs {}
+pub struct CliArgs {
+    #[command(flatten)]
+    pub verbose: Verbosity,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::Parser;
 use rust_cli_template::cli::CliArgs;
+use tracing_log::AsTrace;
 
 fn main() {
     let args = CliArgs::parse();
+
+    tracing_subscriber::fmt()
+        .with_max_level(args.verbose.log_level_filter().as_trace())
+        .init();
 }
