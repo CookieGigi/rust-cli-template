8d8a9755a5710f7e0ac4db982cdff0f120d56304
Cookiegigi <gcreusot@pm.me>
2024-04-27 17:11:46 +0700

    Fix integration test to actually test the command

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,8 +1,9 @@
-use assert_cmd::{cargo::CommandCargoExt, Command};
+use assert_cmd::Command;
 
 #[test]
 fn cli_test() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("rust-cli-template")?;
 
+    cmd.assert().success();
     Ok(())
 }
